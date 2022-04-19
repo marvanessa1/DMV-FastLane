d4c9af8708317f91a8292746352205456d8d7309
maddy <maddykimborowicz@gmail.com>
2022-04-18 20:44:32 -0700

    add ticket remove ticket mutations

---
+++ client/src/utils/services.js
@@ -0,0 +1,14 @@
+export const services = [
+  {
+    name: "Renew License",
+    time: 20,
+  },
+  {
+    name: "Driver's Test",
+    time: 30,
+  },
+  {
+    name: "Pay a bill",
+    time: 5,
+  },
+];

ad2a1c41efe3eb4a3ab5d2359fa920d05b7dcbf9
Emma Mondul <emondul@gmail.com>
2022-04-20 21:29:39 -0700

    adding services

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -13,6 +13,54 @@ export const services = [
   },
   {
     name: "Enhanced Driver License",
+    time: 15,
+  },
+  {
+    name: "ID card",
+    time: 15,
+  },
+  {
+    name: "Update License",
+    time: 5,
+  },
+  {
+    name: "Replace License",
+    time: 5,
+  },
+  {
+    name: "Commercial Driver License",
+    time: 20,
+  },
+  {
+    name: "Suspended License",
+    time: 15,
+  },
+  {
+    name: "Register Vehicle",
+    time: 25,
+  },
+  {
+    name: "Buy a Vehicle",
+    time: 20,
+  },
+  {
+    name: "Sell a Vehicle",
+    time: 25,
+  },
+  {
+    name: "License plates",
+    time: 20,
+  },
+  {
+    name: "Professional License",
     time: 10,
+  },
+  {
+    name: "Renew Professional License",
+    time: 10,
+  },
+  {
+    name: "Background Check",
+    time: 30,
   },
 ];

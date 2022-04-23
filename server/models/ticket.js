e29472c234237b284b38340bb0f1fed94567ba05
maddy <maddykimborowicz@gmail.com>
2022-04-22 23:06:18 -0700

    got addticket working in graphql and added removeticket to typedefs and resolvers

--- server/models/ticket.js
+++ server/models/ticket.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const ticketSchema = new Schema({
     firstName: {
@@ -22,30 +21,8 @@ const ticketSchema = new Schema({
     complete: {
         type: Boolean,
         default: false,
-    },
-    ticketId: {
-        type: String,
-        required: true,
-      },
-    userID: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
     }
 });
-
-ticketSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-
-    next();
-});
-
-ticketSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
-};
 
 const Ticket = model('Ticket', ticketSchema);
 
